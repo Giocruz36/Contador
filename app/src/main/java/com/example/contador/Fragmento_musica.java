b7a229d4833ab55ac72304aa97d8cb64e6c5fef5
Giovanni Alexander Cruz <84064807+Giocruz36@users.noreply.github.com>
2022-02-11 12:12:53 +0100

    avance de sonido de muscia en la app

--- app/src/main/java/com/example/contador/Fragmento_musica.java
+++ app/src/main/java/com/example/contador/Fragmento_musica.java
@@ -1,5 +1,6 @@
 package com.example.contador;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class Fragmento_musica extends Fragment {
 
-
+    private boolean encendida;
+    private ImageView botonMusica;
     public Fragmento_musica() {
-        // Required empty public constructor
     }
 
     @Override
@@ -20,10 +22,34 @@ public class Fragmento_musica extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    public void Apagamusica(){
+        botonMusica.setImageResource(R.drawable.musica);
+        Intent miReproductor=new Intent(getActivity(),Servicios.class);
+        getActivity().stopService(miReproductor);
+        encendida=!encendida;
+    }
+    public void Enciendemusica(){
+        botonMusica.setImageResource(R.drawable.musica2);
+        Intent miReproductor=new Intent(getActivity(),Servicios.class);
+        getActivity().startService(miReproductor);
+        encendida=!encendida;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmento_musica, container, false);
+        View fragmento= inflater.inflate(R.layout.fragment_fragmento_musica, container, false);
+        botonMusica=(ImageView)fragmento.findViewById(R.id.muscia);
+        if(encendida){
+        botonMusica.setImageResource(R.drawable.musica2);         }
+        botonMusica.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View vista){
+                if (encendida){
+                    Apagamusica();
+                }else{
+                    Enciendemusica();
+        }
+            }
+        });
+        return fragmento;
     }
 }
