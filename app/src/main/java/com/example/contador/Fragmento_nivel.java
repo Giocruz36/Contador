887f01f3f1f2c3137a96f43c7373764f2c09db16
Giovanni Alexander Cruz <giovannialexandercruzosegueda@gmail.com>
2022-02-23 19:46:56 +0100

    hacer la herramienta nivel

--- app/src/main/java/com/example/contador/Fragmento_nivel.java
+++ app/src/main/java/com/example/contador/Fragmento_nivel.java
@@ -1,23 +1,62 @@
 package com.example.contador;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragmento_nivel extends Fragment {
+public class Fragmento_nivel extends Fragment implements SensorEventListener {
+    private NivelPantalla pantalla;
+    private SensorManager miManager;
+    private Sensor miSensor;
+    private int dimenw,dimenh;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public Fragmento_nivel(){}
+
+    public void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        miManager=(SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+        miSensor=miManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        dimenw = metrics.widthPixels;
+        dimenh = metrics.heightPixels;
+        pantalla = new NivelPantalla(getActivity(),dimenw,dimenh);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fragmento_nivel, container, false);
+        View fragmento = pantalla;
+        return fragmento;
+    }
+    public void onResume(){
+        super.onResume();
+        miManager.registerListener(this,miSensor,SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    public void onPause(){
+        super.onPause();
+        miManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        pantalla.angulos(event.values);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int acurracy) {
+
     }
 }
--- app/src/main/java/com/example/contador/NivelPantalla.java
+++ app/src/main/java/com/example/contador/NivelPantalla.java
@@ -1,59 +1,67 @@
 package com.example.contador;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Typeface;
-import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.WindowManager;
+import android.graphics.drawable.BitmapDrawable;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.content.ContextCompat;
 
 public class NivelPantalla extends AppCompatImageView {
     int posLeft,posTop,posXY,dimenw,dimenh,radio;
+    float[] angulos;
+    Bitmap fondo, burbuja;
 
-
-    /*public void onCreate (Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(new NivelPantalla(this));
-    }*/
-
-    public NivelPantalla(Context contexto) {
+    public NivelPantalla(Context contexto, int dw, int dh) {
         super(contexto);
+        dimenh=dh;
+        dimenw=dw;
         posLeft=100;
         posTop=75;
-        posXY=500;
-        radio=dimenw/200-posLeft;
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        dimenw=metrics.widthPixels;
-        dimenh=metrics.heightPixels;
+        posXY=100;
+        radio=dimenh/2-posLeft;
+        fondo = iniciaFondo();
+        angulos=new float[2];
+        angulos[0]=0;
+        angulos[1]=0;
+        BitmapDrawable bola = (BitmapDrawable) ContextCompat.getDrawable(contexto,R.drawable.burbuja);
+        burbuja = bola.getBitmap();
+        burbuja=Bitmap.createScaledBitmap(burbuja,radio/4,radio/4,true);
     }
-
-    private WindowManager getWindowManager() {
-     return null;
+    public void angulos(float[]angulos){
+        this.angulos=angulos;
+        invalidate();
+    }
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(dimenw, dimenh);
     }
 
     protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawBitmap(fondo,0,0,null);
+        int posX = radio+(int)(angulos[0]/10*radio)+posLeft/2;
+        int posY = radio-(int)(angulos[1]/10*radio)+(dimenh/4);
+        canvas.drawBitmap(burbuja,posX,posY,null);
+    }
+
+    private Bitmap iniciaFondo() {
+        Bitmap.Config conf = Bitmap.Config.ARGB_4444;
+        Bitmap fondo = Bitmap.createBitmap(dimenw,dimenh,conf);
+        Canvas canvas = new Canvas(fondo);
         Paint lapiz = new Paint();
         lapiz.setColor(Color.GRAY);
         lapiz.setStyle(Paint.Style.FILL);
-        canvas.drawRect(0,0,dimenw,dimenh,lapiz);
+        canvas.drawRect(0, 0, dimenw, dimenh, lapiz);
         lapiz.setColor(Color.RED);
         lapiz.setStrokeWidth(15);
         lapiz.setStyle(Paint.Style.STROKE);
-        canvas.drawCircle(dimenw/2,dimenw/2,radio,lapiz);
-        lapiz.setColor(Color.BLACK);
-        lapiz.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(dimenw/2,dimenw/2,radio,lapiz);
-        lapiz.setColor(Color.RED);
-        lapiz.setStyle(Paint.Style.FILL);
-        canvas.drawCircle(dimenw/2,dimenw/2,radio/4,lapiz);
-        canvas.drawLine(posLeft,dimenh/2,radio+dimenw/2,dimenh/2,lapiz);
-        canvas.drawLine(dimenw/2,dimenh/2-radio,dimenw/2,dimenh/2+radio,lapiz);
-        Path miTrazo = new Path();
+        canvas.drawCircle(dimenw/2, dimenh/2, radio, lapiz);
+        Path miTrazo =new Path();
         miTrazo.addCircle(dimenw/2,dimenh/2,radio,Path.Direction.CCW);
         canvas.drawPath(miTrazo,lapiz);
         lapiz.setStyle(Paint.Style.FILL);
@@ -61,5 +69,14 @@ public class NivelPantalla extends AppCompatImageView {
         lapiz.setTextSize(100);
         lapiz.setTypeface(Typeface.SANS_SERIF);
         canvas.drawTextOnPath("Nuestro nivel",miTrazo,dimenh,100,lapiz);
+        lapiz.setColor(Color.BLACK);
+        lapiz.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(dimenw/2, dimenh/2, radio, lapiz);
+        lapiz.setColor(Color.RED);
+        lapiz.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(dimenw/2, dimenh/2, radio/4, lapiz);
+        canvas.drawLine(posLeft,dimenh/2, radio+dimenw/2, dimenh/2, lapiz);
+        canvas.drawLine(dimenw/2,dimenh/2-radio, dimenw/2, dimenh/2+radio, lapiz);
+        return fondo;
     }
 }
