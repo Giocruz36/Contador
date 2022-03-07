f1123c1fcfd9612cac09753e4ec6f5f31f88da40
Giovanni Alexander Cruz <giovannialexandercruzosegueda@gmail.com>
2022-03-07 20:54:42 +0100

    podometro

--- app/src/main/java/com/example/contador/Pedometer.java
+++ app/src/main/java/com/example/contador/Pedometer.java
@@ -11,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
